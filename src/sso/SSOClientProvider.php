1ecf429a15f47338e04ebeb3eea432b87ec48945
Azharuddin Arrosis <azharoce@gmail.com>
2025-02-26 14:42:04 +0700

    tenan

--- src/sso/SSOClientProvider.php
+++ src/sso/SSOClientProvider.php
@@ -3,75 +3,116 @@
 namespace Pathgrow\SSO;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\ServiceProvider;
 
 use App\Models\User;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Http;
 use InvalidArgumentException;
+use Illuminate\Http\JsonResponse;
+
 
 class SSOClientProvider extends ServiceProvider
 {
+    public static $apps_id;
+    public static $secret_key;
+    public static $sso_redirect_uri;
+    public static $app_url;
+    public static $client_id;
+    public static $tenan_id;
 
+    public function __construct()
+    {
+        self::$tenan_id = getenv('TENAN_ID') ? ENV('TENAN_ID') : getenv('TENAN_ID');
+        self::$apps_id = getenv('APP_ID') ? ENV('APP_ID') : getenv('APP_ID');
+        self::$client_id = getenv('CLIENT_ID') ? ENV('CLIENT_ID') : getenv('CLIENT_ID');
+        self::$secret_key = getenv('SECRET_KEY') ? ENV('SECRET_KEY') : getenv('SECRET_KEY');
+        self::$sso_redirect_uri = getenv('APP_URL') . "/sso/callback" ? ENV('APP_URL') . "/sso/callback" : getenv('APP_URL') . "/sso/callback";
+        self::$app_url = getenv('APP_URL') ? ENV('APP_URL') : getenv('APP_URL');
+    }
+    public static function checkingApps(Request $request)
+    {
+        $response = Http::withHeaders([
+            "Accept" => "application/json",
+            "apps-id" => self::$apps_id,
+            "client-id" => self::$client_id,
+            "tenan-id" => self::$tenan_id,
+        ])->post(env('SSO_HOST') . "/api/apps");
+        $apps_status = $response->json();
+        if ($apps_status['status'] == 1) {
+            return true;
+        };
+        return false;
+    }
     public static function login(Request $request)
     {
+        $checkingApps = self::checkingApps($request);
+        if ($checkingApps) {
+            return redirect("/")->withErrors("Maaf aplikasi anda belum diaktifkan");
+        }
         $request->session()->put("state", $state = Str::random(40));
         $query = http_build_query([
-            "client_id" => env('SSO_CLIENT_ID'),
-            "redirect_uri" => env('SSO_REDIRECT_URI'),
+            "client_id" => self::$client_id,
+            "redirect_uri" => self::$sso_redirect_uri,
             "response_type" => "code",
             "scope" => "",
-            "state" => $state
+            "state" => $state,
         ]);
+
         return redirect(env('SSO_HOST') . "/oauth/authorize?" . $query);
     }
-
-    public static function getAuthLink(Request $request): JsonResponse
+    public static function loginApps(Request $request): JsonResponse
     {
-        $request->session()->put("state", $state = Str::random(40));
-        $query = http_build_query([
-            "client_id" => env('SSO_CLIENT_ID'),
-            "redirect_uri" => env('SSO_REDIRECT_URI'),
-            'response_type' => 'code',
-            'scope' => '',
-            'state' => $state,
+
+        $response = Http::withHeaders([
+            "Accept" => "application/json",
+        ])->post(env('SSO_HOST') . "/api/login", [
+            'email' => $request->email,
+            'password' => $request->password
         ]);
 
-        return response()->json(['authorize_url' => env('SSO_HOST') . '/oauth/authorize?' . $query, 'state' => $state], 200);
-    }
+        $apps_status = $response->json();
+        if (array_key_exists('error', $apps_status)) {
+            return response()->json(['message' => 'Login Gagal', 'status' => 'error']);
+        }
+        $response = Http::withHeaders([
+            "Accept" => "application/json",
+            "Authorization" => "Bearer " . $apps_status['data']['token'],
+            "client-id" => config('app.client_id'),
+            "apps-id" => config('app.app_id'),
+            "tenan-id" => config('app.tenan_id'),
+        ])->get(env('SSO_HOST') . "/api/user");
 
-    private function getAuthToken($state, $code)
-    {
-        throw_unless(strlen($state) > 0 && $state, InvalidArgumentException::class);
+        $dataUser = $response->json();
+        try {
+            $email = $dataUser["email"];
+        } catch (\Throwable $th) {
+            return redirect("login")->withErrors("Maaf akun ada tidak terdaftar di system SSO Kami.");
+        }
 
-        $response = Http::asForm()->post(
-            env('SSO_HOST') . '/oauth/token',
-            [
-                'grant_type' => 'authorization_code',
-                'client_id' => env('SSO_CLIENT_ID'),
-                'client_secret' => env('SSO_CLIENT_SECRET'),
-                'redirect_url' => env('SSO_REDIRECT_URI'),
-                'code' => $code,
-            ]
-        );
-        return $response->json()['access_token'];
-    }
-    public static function callback_api(Request $request)
-    {
-        $state = $request->session()->pull("state");
-        throw_unless(strlen($state) > 0 && $state == $request->state, InvalidArgumentException::class);
+        $user = User::where('email', $email)->first();
 
-        $response = Http::asForm()->post(env('SSO_HOST') . "/oauth/token", [
-            "grant_type" => "authorization_code",
-            "client_id" => env('SSO_CLIENT_ID'),
-            "client_secret" => env('SSO_CLIENT_SECRET'),
-            "redirect_uri" => env('SSO_REDIRECT_URI'),
-            "code" => $request->code,
-        ]);
-        $request->session()->put($response->json());
-        return $response->json();
+        if (!$user) {
+            $user = new User;
+            $user->name = $dataUser['name'];
+            $user->email = $dataUser['email'];
+            $user->client_id = $dataUser['client_code'];
+            $user->email_verified_at = $dataUser['email_verified_at'];
+            $user->save();
+        }
+
+        $data = array();
+        if ($user) {
+            Auth::login($user);
+            $token = $user->createToken(self::$apps_id)->accessToken;
+            $data['token'] = $token;
+            $data['user'] = $user;
+            $data['message'] = 'Login Berhasil';
+            $data['status'] = 'success';
+        }
+
+        return response()->json($data);
     }
     public static function callback(Request $request)
     {
@@ -80,11 +121,12 @@ class SSOClientProvider extends ServiceProvider
 
         $response = Http::asForm()->post(env('SSO_HOST') . "/oauth/token", [
             "grant_type" => "authorization_code",
-            "client_id" => env('SSO_CLIENT_ID'),
-            "client_secret" => env('SSO_CLIENT_SECRET'),
-            "redirect_uri" => env('SSO_REDIRECT_URI'),
+            "client_id" => self::$client_id,
+            "client_secret" => self::$secret_key,
+            "redirect_uri" => self::$sso_redirect_uri,
             "code" => $request->code,
         ]);
+
         $request->session()->put($response->json());
         return redirect(route("sso.account"));
     }
@@ -94,25 +136,38 @@ class SSOClientProvider extends ServiceProvider
         $response = Http::withHeaders([
             "Accept" => "application/json",
             "Authorization" => "Bearer " . $access_token,
+            "apps-id" => self::$apps_id,
+            "client-id" => self::$client_id,
+            "tenan-id" => self::$tenan_id
         ])->get(env('SSO_HOST') . "/api/user");
 
         $dataUser = $response->json();
         try {
             $email = $dataUser["email"];
         } catch (\Throwable $th) {
-            return redirect("login")->withErrors("Failed get data information");
+            return redirect("login")->withErrors("Maaf akun ada tidak terdaftar di system SSO Kami.");
         }
-
+        if (empty($dataUser['apps_id'])) {
+            return redirect("/")->withErrors("Maaf Akun anda tidak boleh mengakses aplikasi ini.");
+        }
+        if ($dataUser['apps_id'] != self::$apps_id) {
+            return redirect("/")->withErrors("Maaf Akun anda tidak boleh mengakses aplikasi ini.");
+        }
         $user = User::where('email', $email)->first();
 
         if (!$user) {
             $user = new User;
             $user->name = $dataUser['name'];
             $user->email = $dataUser['email'];
+            $user->client_id = $dataUser['client_code'];
             $user->email_verified_at = $dataUser['email_verified_at'];
             $user->save();
         }
         Auth::login($user);
+        if ($user) {
+            $token = $user->createToken(self::$apps_id)->accessToken;
+            $request->session()->put('api_token', $token);
+        }
         $request->session()->put('data', $response->json());
         return redirect("");
     }
